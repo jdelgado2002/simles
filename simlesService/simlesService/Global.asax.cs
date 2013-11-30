d886b28e036b91d028113fbf8eac91142a6c63da
Jose Mauricio Delgado <jdelgado2002@gmail.com>
2013-11-30 13:15:51 -0600

    Adding in memory test user correctly

--- simlesService/simlesService/Global.asax.cs
+++ simlesService/simlesService/Global.asax.cs
@@ -1,44 +1,54 @@
-﻿namespace simlesService {
-    using System;
-    using System.Web;
+﻿using System;
+using System.Web;
+using Funq;
+using ServiceStack.CacheAccess;
+using ServiceStack.CacheAccess.Providers;
+using ServiceStack.ServiceInterface;
+using ServiceStack.ServiceInterface.Auth;
+using ServiceStack.WebHost.Endpoints;
 
-    using Funq;
-
-    using ServiceStack.CacheAccess;
-    using ServiceStack.CacheAccess.Providers;
-    using ServiceStack.ServiceInterface;
-    using ServiceStack.ServiceInterface.Auth;
-    using ServiceStack.WebHost.Endpoints;
-
-    public class Global : HttpApplication {
-        protected void Application_Start(object sender, EventArgs e) {
+namespace simlesService
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
             new SimlesAppHost().Init();
         }
 
-        protected void Session_Start(object sender, EventArgs e) {
+        protected void Session_Start(object sender, EventArgs e)
+        {
         }
 
-        protected void Application_BeginRequest(object sender, EventArgs e) {
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
         }
 
-        protected void Application_AuthenticateRequest(object sender, EventArgs e) {
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
         }
 
-        protected void Application_Error(object sender, EventArgs e) {
+        protected void Application_Error(object sender, EventArgs e)
+        {
         }
 
-        protected void Session_End(object sender, EventArgs e) {
+        protected void Session_End(object sender, EventArgs e)
+        {
         }
 
-        protected void Application_End(object sender, EventArgs e) {
+        protected void Application_End(object sender, EventArgs e)
+        {
         }
 
-        public class SimlesAppHost : AppHostBase {
-            public SimlesAppHost(): base("simles client", typeof(ClientService).Assembly) {
+        public class SimlesAppHost : AppHostBase
+        {
+            public SimlesAppHost() : base("simles client", typeof (ClientService).Assembly)
+            {
             }
 
-            public override void Configure(Funq.Container container) {
-                Plugins.Add(new AuthFeature(() => new AuthUserSession(), new IAuthProvider[] {new BasicAuthProvider(), }));
+            public override void Configure(Container container)
+            {
+                Plugins.Add(new AuthFeature(() => new AuthUserSession(), new IAuthProvider[] {new BasicAuthProvider()}));
                 container.Register<ICacheClient>(new MemoryCacheClient());
                 var userRepository = new InMemoryAuthRepository();
                 container.Register<IUserAuthRepository>(userRepository);
@@ -48,8 +58,17 @@
                 string salt;
 
                 new SaltedHash().GetHashAndSaltString("password", out hash, out salt);
+                //This is how we would add it from a registration page.
                 userRepository.CreateUserAuth(
-                    new UserAuth() { Id = 1, DisplayName = "joselito", Email = "yiyo@hotmail.com", PasswordHash = hash, Salt = salt}, "password");
+                    new UserAuth
+                    {
+                        Id = 1,
+                        UserName = "jdelgado",
+                        DisplayName = "joselito",
+                        Email = "yiyo@hotmail.com",
+                        PasswordHash = hash,
+                        Salt = salt
+                    }, "password");
             }
         }
     }
